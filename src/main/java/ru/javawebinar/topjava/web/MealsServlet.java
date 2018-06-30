5dbd9cbb01b6de72e75e24e8e5546af737b9e861
a.beloglazov <a.beloglazov@tinkoff.ru>
2018-06-30 20:34:09 +0300

    Meal with exceed UI

--- src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealsServlet.java
@@ -1,23 +1,65 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.MealWithExceed;
+import ru.javawebinar.topjava.util.MealsUtil;
 
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
 public class MealsServlet extends HttpServlet {
     private static final Logger log = getLogger(UserServlet.class);
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        log.debug("redirect to meals");
+    List<Meal> meals;
 
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+    @Override
+    public void init() {
+        log.info("Инициализация");
+        meals = Arrays.asList(
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 1000),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
+                new Meal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
+                             );
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        log.debug("Send meas to jsp");
+        ServletContext context = this.getServletContext();
+        RequestDispatcher dispatcher;
+
+        Map<String, MealWithExceed> mealsWithExceeded = getMealsWithExceededMap(meals);
+
+        req.setAttribute("meals", mealsWithExceeded);
+        req.setCharacterEncoding("UTF-8");
+        res.setCharacterEncoding("UTF-8");
+
+        dispatcher = context.getRequestDispatcher("/meals.jsp");
+        dispatcher.include(req, res);
+    }
+
+    private static List<MealWithExceed> mapMealsToMealsWithExceed(List<Meal> meals) {
+        log.debug("Mapping Meals to Meals with exceed");
+        final Integer CALORIES_LIMIT = 2000;
+        Map<LocalDate, Integer> caloriesSumByDate = new HashMap<>();
+        meals.forEach(meal -> caloriesSumByDate.merge(meal.getDate(), meal.getCalories(), Integer::sum));
+        return meals.stream().map(x -> MealsUtil.createWithExceed(x, caloriesSumByDate.get(x.getDate()) > CALORIES_LIMIT)).collect(Collectors.toList());
+    }
+
+    private static Map<String, MealWithExceed> getMealsWithExceededMap(List<Meal> meals) {
+        log.debug("Get map of meals with exceed");
+        return mapMealsToMealsWithExceed(meals).stream().collect(Collectors.toMap(x -> x.getDateTime().toString().replace("T", " "), x -> x));
     }
 }
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -1,4 +1,6 @@
-<%--
+<%@ page import="ru.javawebinar.topjava.model.MealWithExceed" %>
+<%@ page import="java.util.List" %>
+<%@ page import="java.util.Map" %><%--
   Created by IntelliJ IDEA.
   User: a.beloglazov
   Date: 30.06.2018
@@ -13,5 +15,23 @@
 <body>
 <h3><a href="index.html">Home</a></h3>
 <h2>Meals</h2>
+
+<table width="400">
+    <thead>
+    <th>Date / Time</th>
+    <th>Description</th>
+    <th>Calories</th>
+    </thead>
+    <%
+        Map<String, MealWithExceed> meals = (Map<String, MealWithExceed>) request.getAttribute("meals");
+        for (Map.Entry<String, MealWithExceed> m : meals.entrySet()) {%>
+    <tr bgcolor="aqua">
+        <td><%=m.getKey()%></td>
+        <td><%=m.getValue().getDescription()%></td>
+        <td><%=m.getValue().getCalories()%>
+        <td><%=m.getValue().isExceed()%></td>
+    </tr>
+    <%}%>
+</table>
 </body>
 </html>
