a875a198af919bd18614ae133ca324e99cb690d7
a.beloglazov <a.beloglazov@tinkoff.ru>
2018-07-07 18:53:08 +0300

    (список пользователей возвращать отсортированным по имени)

--- src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/mock/InMemoryUserRepositoryImpl.java
@@ -3,15 +3,16 @@ package ru.javawebinar.topjava.repository.mock;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.model.AbstractNamedEntity;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
-
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Repository
 public class InMemoryUserRepositoryImpl implements UserRepository {
     private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepositoryImpl.class);
+    private List<User> repository = new ArrayList<>();
 
     @Override
     public boolean delete(int id) {
@@ -34,7 +35,7 @@ public class InMemoryUserRepositoryImpl implements UserRepository {
     @Override
     public List<User> getAll() {
         log.info("getAll");
-        return Collections.emptyList();
+        return repository.stream().sorted(Comparator.comparing(AbstractNamedEntity::getName)).collect(Collectors.toList());
     }
 
     @Override
